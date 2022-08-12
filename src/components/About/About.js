7f808201ba62f3b3a70a32f31d94b729c2945a52
nkirti28 <nalawade.kirti@gmail.com>
2022-08-11 22:20:02 -0500

    About n Projects component added'

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,38 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Particle from "../Particles";
+import Aboutcard from "./AboutCard";
+import laptopImg from "../../assets/office.png";
 
 function About() {
   return (
-    <section>
-      <h1 id="about">Who am I?</h1>
-    </section>
+    <Container fluid className="about-section">
+      <Particle />
+      <Container>
+        <Row style={{ justifyContent: "center", padding: "10px" }}>
+          <Col
+            md={7}
+            style={{
+              justifyContent: "center",
+              paddingTop: "30px",
+              paddingBottom: "50px",
+            }}
+          >
+            <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
+              Know Who <strong className="purple">I'M</strong>
+            </h1>
+            <Aboutcard />
+          </Col>
+          <Col
+            md={5}
+            style={{ paddingTop: "120px", paddingBottom: "50px" }}
+            className="about-img"
+          >
+            <img src={laptopImg} alt="about" className="img-fluid" />
+          </Col>
+        </Row>
+      </Container>
+    </Container>
   );
 }
 
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+import runBuddy from "../../assets/projects/hero-bg.jpg";
+import wonderDestination from "../../assets/projects/Wonder Destinations.png";
+import smartBook from "../../assets/projects/Home_page.PNG";
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={runBuddy}
+              title="RunBuddy"
+              description="workout tracker application"
+              ghLink="https://github.com/nkirti28/run-buddy"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={wonderDestination}
+              title="Wonder Destinations"
+              description="Travel guide application"
+              ghLink="https://github.com/nkirti28/wonder-destinations"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={smartBook}
+              title="Smart Book"
+              description="Online Book store application"
+              ghLink="https://github.com/nkirti28/smart-book"
+              demoLink="https://smartest-book.herokuapp.com/"
+            />
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
